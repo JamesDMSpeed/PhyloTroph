8e53364dbbb42ef96a8a091c071166ce9f4369e1
JamesDMSpeed <james.speed@ntnu.no>
2019-10-30 21:58:53 +0100

    Looking at different definitions of omnivores

--- Histogram.R
+++ Histogram.R
@@ -19,9 +19,11 @@ NorSpecies <- TraitData %>%
 #NamesM <- as.character(NorMammals$Binomial.1.2)
 #NamesM <-substring(NamesM,2)
 
-#Only terrestrial species (omit also terrestrial + marine spp e.g. seals)
-NorTerrSpecies<-NorSpecies[NorSpecies$Marine==0,]
-dim(NorTerrSpecies)#58 species
+#Only terrestrial species (omit also seals based on taxonomy)
+NorTerrSpecies<-NorSpecies[NorSpecies$Family.1.2!='Phocidae' & NorSpecies$Terrestrial==1,]
+dim(NorTerrSpecies)#47 species
+NorTerrSpecies[,1:3] # Note this includes humans!
+
 ###HISTOGRAMS###
 #Frequency
 hist(NorTerrSpecies$Diet.Plant,
@@ -38,6 +40,20 @@ hist(NorTerrSpecies$Diet.Plant,
 hist(NorTerrSpecies$Diet.Invertebrate)
 
 hist(NorTerrSpecies$Diet.Vertebrate)
+
+#Check species with not more than 60% of any diet
+View(NorTerrSpecies[NorTerrSpecies$Diet.Vertebrate<=60 
+               & NorTerrSpecies$Diet.Invertebrate<=60
+               & NorTerrSpecies$Diet.Plant<=60,]) #3
+#Check species with not more than 50% of any diet
+View(NorTerrSpecies[NorTerrSpecies$Diet.Vertebrate<=50 
+                                & NorTerrSpecies$Diet.Invertebrate<=50
+                                & NorTerrSpecies$Diet.Plant<=50,])#1
+#Check species with not more than 70% of any diet
+View(NorTerrSpecies[NorTerrSpecies$Diet.Vertebrate<=70 
+                                & NorTerrSpecies$Diet.Invertebrate<=70
+                                & NorTerrSpecies$Diet.Plant<=70,])#14
+
 
 library(MASS)
 truehist(NorTerrSpecies$Diet.Plant)
