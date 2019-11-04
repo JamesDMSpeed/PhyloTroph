6413fb62b97e5a1117878519287e2b167304f4dc
damarismm <damaris.m.matten@ntnu.no>
2019-11-04 21:30:34 +0100

    Norwegian mammal (IUCN) rasterized

---
+++ mammalIUCN.R
@@ -0,0 +1,32 @@
+
+library(raster)
+library(rgdal)
+library(raster)
+library(sf)
+library(fasterize)
+
+#Norway Mainland
+mamN <- readOGR("NorwayMainland.shp")
+mamN1 <- mamN
+mamTrans1<-spTransform(mamN1,crs("+proj=utm +zone=33 +ellps=GRS80 +units=m +no_defs"))
+e<-extent(mamTrans1)
+s<-raster(e, resolution=10000, crs=(mamTrans1))
+mamSF <- st_as_sf(mamTrans1)
+#one layer per species, so here 55 distribution maps
+stackedDistributionMaps<-fasterize(mamSF,s,by="BINOMIAL")
+plot(stackedDistributionMaps)
+#merge polygons per species for species richness map
+speciesPoly<-aggregate(mamTrans1,by="BINOMIAL")
+speciesPoly2<-st_as_sf(speciesPoly)
+#count number of overlapping polygons (which is 1 per species so counting gives richness)
+richnessMap <- fasterize(speciesPoly2,s,fun="count",field="BINOMIAL")
+plot(richnessMap)
+
+
+#Mammals Norway and Svalbard
+#alread rasterized
+mamNS <- load(file="mammalNS.RData")
+plot(mamNS[[richnessMap]])
+plot(mamNS[[2]])
+
+
