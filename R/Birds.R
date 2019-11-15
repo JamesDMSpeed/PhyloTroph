e47faa3769f243e7729d5824c7e6dddd09e496cc
damarismm <damaris.m.matten@ntnu.no>
2019-11-15 10:53:13 +0100

    Bird ranges

---
+++ R/Birds.R
@@ -0,0 +1,23 @@
+
+library(raster)
+library(rgdal)
+library(raster)
+library(sf)
+library(fasterize)
+
+#Norway Mainland
+bird <- readOGR("Norway_bird.shp")
+bird1 <- bird
+birdTrans1<-spTransform(bird1,crs("+proj=utm +zone=33 +ellps=GRS80 +units=m +no_defs"))
+e<-extent(birdTrans1)
+s<-raster(e, resolution=10000, crs=(birdTrans1))
+birdSF <- st_as_sf(birdTrans1)
+#one layer per species, so here 98 distribution maps
+birdstacked<-fasterize(birdSF,s,by="SCINAME")
+plot(birdstacked)
+#merge polygons per species for species richness map
+birdPoly<-aggregate(birdTrans1,by="SCINAME")
+birdPoly2<-st_as_sf(birdPoly)
+#count number of overlapping polygons (which is 1 per species so counting gives richness)
+birdrichness <- fasterize(birdPoly2,s,fun="count",field="SCINAME")
+plot(birdrichness)
