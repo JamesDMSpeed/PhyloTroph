07eaafdf7926ef069b13af59e52357e87bb9c308
damarismm <damaris.m.matten@ntnu.no>
2020-01-06 13:38:06 +0100

    updated script for mammal ranges

--- R/mammalIUCN.R
+++ R/mammalIUCN.R
@@ -1,18 +1,17 @@
 
 library(raster)
 library(rgdal)
-library(raster)
 library(sf)
 library(fasterize)
 
-#Norway Mainland
-mamN <- readOGR("NorwayMainland.shp")
+
+mamN <- readOGR("NorwayNew_.shp")
 mamN1 <- mamN
 mamTrans1<-spTransform(mamN1,crs("+proj=utm +zone=33 +ellps=GRS80 +units=m +no_defs"))
 e<-extent(mamTrans1)
 s<-raster(e, resolution=10000, crs=(mamTrans1))
 mamSF <- st_as_sf(mamTrans1)
-#one layer per species, so here 55 distribution maps
+#one layer per species
 stackedDistributionMaps<-fasterize(mamSF,s,by="BINOMIAL")
 plot(stackedDistributionMaps)
 #merge polygons per species for species richness map
@@ -21,12 +20,3 @@ speciesPoly2<-st_as_sf(speciesPoly)
 #count number of overlapping polygons (which is 1 per species so counting gives richness)
 richnessMap <- fasterize(speciesPoly2,s,fun="count",field="BINOMIAL")
 plot(richnessMap)
-
-
-#Mammals Norway and Svalbard
-#alread rasterized
-mamNS <- load(file="mammalNS.RData")
-plot(mamNS[[richnessMap]])
-plot(mamNS[[2]])
-
-
