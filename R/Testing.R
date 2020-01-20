5c04b0c9dc89e0f8c42600ec2a29c629afd02e49
kjerstrs <kjerstrs@stud.ntnu.no>
2020-01-20 16:37:34 +0100

    Removing some marine mammals

--- R/Testing.R
+++ R/Testing.R
@@ -7,10 +7,23 @@ library(fasterize)
 library(ggplot2)
 library(leaflet)
 library(tmap)
+library(dplyr)
+
+#Problems with reading in files
+#repN <- readOGR("C:/Phylotroph/Data/Ranges/Reptiles", "NorwayReptiles")
+#repN <- readOGR(dsn = ".", layer = "NorwayReptiles")
+#repN <- readOGR(".","NorwayReptiles")
+#amphN <- readOGR ('Data/Ranges/Amphibians/','amphibian_Norway_IUCN')
 
 #Reading shapefile and layer into spatial vector object
 mamN <- readOGR("C:/Phylotroph/Data/Ranges/Mammals")
 mamN1 <- mamN
+
+List <- c('Pusa hispida','Cystophora cristata','Halichoerus grypus','Phoca vitulina','Erignathus barbatus')
+#mamN1 <- filter(mamN1@data$BINOMIAL, !(BINOMIAL %in% List))
+mamN1 <-mamN1[mamN1@data$BINOMIAL != c('Halichoerus grypus','Phoca vitulina'),]
+mamN1 <-mamN1[mamN1@data$BINOMIAL != c('Cystophora cristata', 'Erignathus barbatus'),]
+mamN1 <-mamN1[mamN1@data$BINOMIAL != 'Pusa hispida',]
 
 #Transformation between datum(s) and conversion between projections, crs = coordinate reference system
 mamTrans1<-spTransform(mamN1,crs("+proj=utm +zone=33 +ellps=GRS80 +units=m +no_defs"))
@@ -20,11 +33,12 @@ e<-extent(mamTrans1)
 #st_as_sf: convert object to sf-object (dataframe w/attributes and geometry)
 s<-raster(e, resolution=20000, crs=(mamTrans1))
 mamSF <- st_as_sf(mamTrans1)
+#Filter out species from list in the sf-object
+#mamSF <- filter(mamSF, !(BINOMIAL %in% List))
 
 #one layer per species (sf, raster)
 stackedDistributionMaps<-fasterize(mamSF,s,by="BINOMIAL")
 plot(stackedDistributionMaps)
-
 
 #merge polygons per species for species richness map, unioning geometries 
 speciesPoly<-aggregate(mamTrans1,by="BINOMIAL")
@@ -34,27 +48,23 @@ speciesPoly2<-st_as_sf(speciesPoly)
 richnessMap <- fasterize(speciesPoly2,s,fun="count",field="BINOMIAL")
 plot(richnessMap)
 
-#Seems like the richnessmap is including areas outside the coastline of Norway? Would want to
-#crop it. Or Are the pale areas part of the land? 
+#Get map of Norway and check the projection
+norway<-getData('GADM',level=0,country='NOR')
+plot(norway)
+#change projetction to the same as mammal-map
+norwayTrans <- spTransform(norway,crs("+proj=utm +zone=33 +ellps=GRS80 +units=m +no_defs"))
+extent(norwayTrans)
+extent(richnessMap) 
+
+###TESTING PACKAGE TMAP###
+qtm(richnessMap)
+mammap <- qtm(richnessMap, fill = "BINOMIAL")
+
+#Stacking tmap elements
 
 #assess the species list: mamN@data$BINOMIAL
 #summary(richnessMap) - 
 #bbox(richnessMap)- explore the bounding area of any spatial object
 #proj4string(richnessMap)- explore the projection system of any spatial object
-
 #class(mamN)
-#shape <- read_sf(dsn = "C:/Phylotroph/Data/Ranges/Mammals", layer = "NorwayNew_")
-#Amph <-read_sf(dsn = "C:/Phylotroph/Data/Ranges/Amphibians", layer = "amphibian_Norway_IUCN")
-#Just get a lot of error messages 
-#Amphibians<-readOGR("C:/Phylotroph/Data/Ranges/Amphibians")
-
-##leaflet/tmap
-#qtm(mamN)
-mammap <- qtm(richnessMap)
-
-
-
-#Get map of Norway
-norway<-getData('GADM',level=0,country='NOR')
-#Create clipping polygon
-norwayTrans <- spTransform(norway,crs("+proj=utm +zone=33 +ellps=GRS80 +units=m +no_defs"))
+#Terrmam <- filter(mamN1@data, !(BINOMIAL %in% List))
